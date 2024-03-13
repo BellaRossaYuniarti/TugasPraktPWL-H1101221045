ef26422655dfd27422f31696b8601e9a38933939
BellaRossa <160710297+BellaRossaYuniarti@users.noreply.github.com>
2024-03-13 13:55:40 +0000

    Initial commit

---
+++ resources/views/register.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Register</title>
+</head>
+<body>
+  <div>
+    <h1>Register</h1>
+    <form action="#" method="post">
+      <input type="text" name="name" placeholder="Name" required><br>
+      <input type="email" name="email" placeholder="Email" required><br>
+      <input type="password" name="password" placeholder="Password" required><br>
+      <input type="password" name="confirm_password" placeholder="Confirm Password" required><br><br>
+      <input type="submit" value="Register">
+    </form>
+  </div>
+</body>
+</html>
